31b83cddf21f244425e26b916daa55535e66984b
Tasssya <tasya.go@gmail.com>
2025-01-27 20:45:22 +0100

    Add files via upload

---
+++ AQA_Java_TatianaGorelova/Circle.java
@@ -0,0 +1,19 @@
+package Lesson6;
+
+public class Circle implements Figure {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ AQA_Java_TatianaGorelova/Triangle.java
@@ -0,0 +1,25 @@
+package Lesson6;
+
+public class Triangle implements Figure {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    @Override
+    public double calculateArea() {
+        double semiPerimeter = calculatePerimeter() / 2;
+        return Math.sqrt(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB) * (semiPerimeter - sideC));
+    }
+}
+
