6b00a99d2f2390aa8918dd57587bd30215b5ea58
Tatyana <tasya.go@gmail.com>
2025-01-31 20:05:14 +0100

    Добавлены файлы для Lesson8

---
+++ AQA_Java_TatianaGorelova/src/main/java/Lesson8/MyArrayDataExeption.java
@@ -0,0 +1,7 @@
+package Lesson8;
+
+public class MyArrayDataExeption extends Exception{
+    public MyArrayDataExeption(String string){
+
+    }
+}
---
+++ AQA_Java_TatianaGorelova/src/main/java/Lesson8/MyArraySizeExeption.java
@@ -0,0 +1,7 @@
+package Lesson8;
+
+public class MyArraySizeExeption extends Exception{
+    public MyArraySizeExeption() {
+
+    }
+}
