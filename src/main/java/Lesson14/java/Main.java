37006fee04fd1908962f7c65a13fb30b4746ae76
Tatyana <tasya.go@gmail.com>
2025-02-24 20:50:12 +0100

    Добавлен автотест FormTest для Lesson14

---
+++ src/main/java/Lesson14/java/Main.java
@@ -0,0 +1,18 @@
+package Lesson14.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.WebDriver;
+
+
+public class Main {
+    public static void main (String[]args){
+
+
+    }
+}
